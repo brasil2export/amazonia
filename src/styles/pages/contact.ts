c3ee34bb0b48ab31da7313bd309376e3103b0468
brasil2export <>
2021-02-16 09:26:12 -0300

    input 100%

--- src/styles/pages/contact.ts
+++ src/styles/pages/contact.ts
@@ -219,6 +219,7 @@ export const Container = styled.div`
       border: none;
       border-bottom: 1px solid black;
       background-color: transparent;
+      width:100%;
   
       &:focus{
         border: none;
